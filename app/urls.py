8248681c6cb48d60647f7d9e8d10d7e35b1188bc
VinayKanase <vinaykanse9451@gmail.com>
2022-04-10 03:31:21 +0530

    feat: Add index, search, login, signup, new routes and controllers

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    path('search/', views.search, name='search'),
+    path('login/', views.login, name='login'),
+    path('signUp/', views.signUp, name='signUp'),
+
+
+    # Login Required
+    path('new/', views.new, name='new'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -5,4 +5,36 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Hello World')
+    """
+        Controller for index or home page
+    """
+    return render(request, 'index.html')
+
+
+def search(request):
+    """
+        Controller for search scholarships page
+    """
+    return HttpResponse("Search Page")
+
+
+def new(request):
+    """
+        Controller to add new scholarships
+        Authorized to logged in users only 
+    """
+    return HttpResponse("New")
+
+
+def login(request):
+    """
+        Controller to login page
+    """
+    return HttpResponse("Login")
+
+
+def signUp(request):
+    """
+        Controller to sign up new user
+    """
+    return HttpResponse("sign up")
